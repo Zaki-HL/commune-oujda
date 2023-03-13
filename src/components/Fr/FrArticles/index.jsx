f8641ce79a5551f6877dcce22f77648201a66b95
AyaZargtouni <a.zargtouni@gmail.com>
2023-03-14 00:00:52 +0100

    rfrjbhb

--- src/components/Fr/FrArticles/index.jsx
+++ src/components/Fr/FrArticles/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './index.scss';
-import aziz from '/imgs/aziz.jpg';
 import imgArticle2 from '/imgs/article2.png';
 import imgEvenement1 from '/imgs/evenement1.jpeg';
 import imgImage1 from '/imgs/image1.jpeg';
@@ -21,7 +20,7 @@ const FrArticles = () => {
 			<div className='row mt-5 align-items-center'>
 
 				{/* Start Article 1 */}
-				<div className='col-md-4'>
+				<div className='col-sm-12 col-md-6 col-lg-4'>
 					<div className='card bg-dark text-white'>
 						<img className='card-img h-100 custom-filter' src={imgArticle2} alt='Card image' />
 						<div className='card-img-overlay d-flex flex-column justify-content-end'>
@@ -33,7 +32,7 @@ const FrArticles = () => {
 				{/* End Article 1 */}
 
 				{/* Start Article 2 */}
-				<div className='col-md-4'>
+				<div className='col-sm-12 col-md-6 col-lg-4'>
 					<div className='card bg-dark text-white'>
 						<img className='card-img h-100 custom-filter' src={imgEvenement1} alt='Card image' />
 						<div className='card-img-overlay d-flex flex-column justify-content-end'>
@@ -45,7 +44,7 @@ const FrArticles = () => {
 				{/* End Article 2 */}
 
 				{/* Start Article 3 */}
-				<div className='col-md-4'>
+				<div className='col-sm-12 col-md-6 col-lg-4'>
 					<div className='card bg-dark text-white'>
 						<img className='card-img h-100 custom-filter' src={imgImage1} alt='Card image' />
 						<div className='card-img-overlay d-flex flex-column justify-content-end'>
@@ -63,7 +62,7 @@ const FrArticles = () => {
 			<div className='row mt-5 align-items-center'>
 
 				{/* Start Article 4 */}
-				<div className='col-md-4'>
+				<div className='col-sm-12 col-md-6 col-lg-4'>
 					<div className='card bg-dark text-white'>
 						<img className='card-img h-100 custom-filter' src={imgImage2} alt='Card image' />
 						<div className='card-img-overlay d-flex flex-column justify-content-end'>
@@ -75,7 +74,7 @@ const FrArticles = () => {
 				{/* End Article 4 */}
 
 				{/* Start Article 5 */}
-				<div className='col-md-4'>
+				<div className='col-sm-12 col-md-6 col-lg-4'>
 					<div className='card bg-dark text-white'>
 						<img className='card-img h-100 custom-filter' src={imgImage3} alt='Card image' />
 						<div className='card-img-overlay d-flex flex-column justify-content-end'>
@@ -87,9 +86,9 @@ const FrArticles = () => {
 				{/* End Article 5 */}
 
 				{/* Start Article 6 */}
-				<div className='col-md-4'>
+				<div className='col-sm-12 col-md-6 col-lg-4'>
 					<div className='card bg-dark text-white'>
-						<img className='card-img h-100 custom-filter' src={imgImage4} alt='Card image' />
+						<img className='card-img h-100 custom-filter' src={imgImage4} alt='Card image'/>
 						<div className='card-img-overlay d-flex flex-column justify-content-end'>
 							<p class="card-text">11 mars 2023 à 13:18</p>
 							<h4 class="card-title">Programme ProFuturo : La Fondation BMCE Bank pour l’Education et l’Environnement enrichit son programme d’éducation numérique</h4>
