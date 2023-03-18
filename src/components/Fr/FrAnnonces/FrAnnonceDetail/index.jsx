48f0bd416b0aae2665b5c21e5699ae21ead43acf
AyaZargtouni <a.zargtouni@gmail.com>
2023-03-18 18:43:30 +0100

    aya

--- src/components/Fr/FrAnnonces/FrAnnonceDetail/index.jsx
+++ src/components/Fr/FrAnnonces/FrAnnonceDetail/index.jsx
@@ -1,13 +1,50 @@
+import React from 'react';
+import myFile from "/docs/file1.pdf";
+
 const FrAnnonceDetail = () => {
 	return (
-		<div className="container p-5 bg-success">
-			<h1 className="text-center text-light">French Annonce Detail</h1>
-			<p className="fw-bold">
-				remove the text decoration undreline using bootstrap & add a title and a
-				small paragraph and the pdf file (choose any file you want "try to put
-				it in public/docs")
-			</p>
-			<p className="text-center">good luck</p>
+		<div className="container p-5">
+			<h2 className="text-center"><u>Concours de recrutement : Technicien de 4ème grade ~ Echelle 8</u></h2>
+			<table class="table table-striped mt-4">
+				<tbody>
+					<tr>
+						<th>Collectivité organisatrice :</th>
+						<td>Commune MAKNASSA ACHARQIA (Province Taza )</td>
+					</tr>
+					<tr>
+						<th>Grade :</th>
+						<td>Technicien de 4ème grade ~ Echelle 8</td>
+					</tr>
+					<tr>
+						<th>Spécialités :</th>
+						<td> genie civil/ resaux informatique</td>
+					</tr>
+					<tr>
+						<th>Type de recrutement :	</th>
+						<td>Recrutement statutaire</td>
+					</tr>
+					<tr>
+						<th>Nombre de postes :	</th>
+						<td>2</td>
+					</tr>
+					<tr>
+						<th>Date de publication :	</th>
+						<td>17 février 2023</td>
+					</tr>
+					<tr>
+						<th>Délai de dépôt des candidatures :	</th>
+						<td>13 mars 2023 <p className="text-danger">(Expiré)</p></td>
+					</tr>
+					<tr>
+						<th>Date du concours :	</th>
+						<td>19 mars 2023</td>
+					</tr>
+				</tbody>
+			</table>
+			<div class="row">
+				<h5 className="mt-3">Fichiers attachés :</h5>
+				<embed src={myFile} type="application/pdf" width="500" height="400" />
+			</div>
 		</div>
 	);
 };
