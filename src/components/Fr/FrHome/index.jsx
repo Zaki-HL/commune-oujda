755884a5972f30c799d21cd529e6b9ab0101a66b
MeReda <medredaben14@gmail.com>
2023-04-02 15:28:09 +0000

    testing

--- src/components/Fr/FrHome/index.jsx
+++ src/components/Fr/FrHome/index.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 import FrLanding from './FrLanding';
 import Articles from './Frarticles';
 import FrMap from './FrMap';
-import FrLogoSlider from './FrLogoSlider';
+// import FrLogoSlider from './FrLogoSlider';
 
 const FrHome = () => {
 	return (
 		<>
 			<FrLanding />
 			<Articles />
-			<FrLogoSlider />
+			{/* <FrLogoSlider /> */}
 			<FrMap />
 		</>
 	);
